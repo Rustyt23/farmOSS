4a8eaacf9f6b6a0e975e2a0f10e57a26eec954c1
Paul Weidner <paul.weidner@gmail.com>
2025-04-25 20:49:41 -0700

    Add attributes for quick form plugins

--- modules/core/quick/tests/modules/farm_quick_test/src/Plugin/QuickForm/TestEntityValidation.php
+++ modules/core/quick/tests/modules/farm_quick_test/src/Plugin/QuickForm/TestEntityValidation.php
@@ -5,22 +5,23 @@ declare(strict_types=1);
 namespace Drupal\farm_quick_test\Plugin\QuickForm;
 
 use Drupal\Core\Form\FormStateInterface;
+use Drupal\Core\StringTranslation\TranslatableMarkup;
+use Drupal\farm_quick\Attribute\QuickForm;
 use Drupal\farm_quick\Plugin\QuickForm\QuickFormBase;
 use Drupal\farm_quick\Traits\QuickAssetTrait;
 
 /**
  * Test Entity Validation quick form.
- *
- * @QuickForm(
- *   id = "test_entity_validation",
- *   label = @Translation("Test entity validation quick form"),
- *   description = @Translation("Test entity validation quick form description."),
- *   helpText = @Translation("Test entity validation quick form help text."),
- *   permissions = {
- *     "create test log",
- *   }
- * )
  */
+#[QuickForm(
+  id: 'test_entity_validation',
+  label: new TranslatableMarkup('Test entity validation quick form'),
+  description: new TranslatableMarkup('Test entity validation quick form description.'),
+  helpText: new TranslatableMarkup('Test entity validation quick form help text.'),
+  permissions: [
+    'create test log',
+  ],
+)]
 class TestEntityValidation extends QuickFormBase {
 
   use QuickAssetTrait;
